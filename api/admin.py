5229486c72e0f989f1af1bb8c2b68588a84406c8
capstonegpt <capstonegpt0@gmail.com>
2026-01-25 22:11:28 +0800

    Fix pestcheck settings for Render deployment

--- api/admin.py
+++ api/admin.py
@@ -2,25 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, PestDetection, PestInfo, InfestationReport
 
+# Safely unregister User if it's already registered
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
 
+# Register custom UserAdmin
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name', 'phone', 'created_at']
     list_filter = ['is_staff', 'is_superuser', 'is_active', 'created_at']
     search_fields = ['username', 'email', 'first_name', 'last_name', 'phone']
     ordering = ['-created_at']
-    
+
     fieldsets = BaseUserAdmin.fieldsets + (
         ('Additional Info', {'fields': ('phone', 'created_at')}),
     )
     readonly_fields = ['created_at']
 
-admin.site.register(User)
-admin.site.register(PestDetection)
-admin.site.register(PestInfo)
-admin.site.register(InfestationReport)
-admin.site.register(Farm)
-
+# Register PestDetection admin
 @admin.register(PestDetection)
 class PestDetectionAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'pest_name', 'crop_type', 'severity', 'confidence', 'detected_at']
@@ -28,7 +29,7 @@ class PestDetectionAdmin(admin.ModelAdmin):
     search_fields = ['pest_name', 'user__username', 'address']
     readonly_fields = ['detected_at']
     ordering = ['-detected_at']
-    
+
     fieldsets = (
         ('Detection Info', {
             'fields': ('user', 'image', 'pest_name', 'crop_type')
@@ -44,13 +45,13 @@ class PestDetectionAdmin(admin.ModelAdmin):
         }),
     )
 
-
+# Register PestInfo admin
 @admin.register(PestInfo)
 class PestInfoAdmin(admin.ModelAdmin):
     list_display = ['name', 'scientific_name', 'crop_affected']
     list_filter = ['crop_affected']
     search_fields = ['name', 'scientific_name', 'crop_affected']
-    
+
     fieldsets = (
         ('Basic Information', {
             'fields': ('name', 'scientific_name', 'crop_affected', 'image_url')
@@ -63,7 +64,7 @@ class PestInfoAdmin(admin.ModelAdmin):
         }),
     )
 
-
+# Register InfestationReport admin
 @admin.register(InfestationReport)
 class InfestationReportAdmin(admin.ModelAdmin):
     list_display = ['id', 'detection', 'area_affected', 'is_verified', 'reported_at']
@@ -71,7 +72,7 @@ class InfestationReportAdmin(admin.ModelAdmin):
     search_fields = ['detection__pest_name', 'notes']
     readonly_fields = ['reported_at']
     ordering = ['-reported_at']
-    
+
     fieldsets = (
         ('Report Info', {
             'fields': ('detection', 'area_affected', 'notes')
