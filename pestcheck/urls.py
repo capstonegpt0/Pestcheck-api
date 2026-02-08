ec97753602efa489b1e159784a29ce840bea0b62
capstonegpt <capstonegpt0@gmail.com>
2026-02-08 21:22:12 +0800

    remove expert role

--- pestcheck/urls.py
+++ pestcheck/urls.py
@@ -1,27 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import FileResponse, Http404
-import os
-
-# Media serving function (defined right here in urls.py)
-def serve_media(request, path):
-    """Serve media files in production"""
-    file_path = os.path.join(settings.MEDIA_ROOT, path)
-    
-    if os.path.exists(file_path):
-        return FileResponse(open(file_path, 'rb'))
-    else:
-        raise Http404(f"Media file not found: {path}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    
-    # Serve media files
-    re_path(r'^media/(?P<path>.*)$', serve_media, name='serve_media'),
-]
-
-# Static files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
